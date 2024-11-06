50d44f44265ceef7be8d95eaf727a2bff29eb4a6
Tjoa <100271617+MarcelinoHansSetiaBudi@users.noreply.github.com>
2024-11-06 16:33:58 +0700

    add insert and update salary

--- Controllers/salaryController.js
+++ Controllers/salaryController.js
@@ -28,6 +28,77 @@ const index = async (req, res) => {
     }
 }
 
+const show = async (req, res) => {
+    const { id } = req.params;
+    try{
+        let data  = await models.Salaries.findByPk(id);
+
+        if(data){
+            return res.status(200).json({
+                status: "success",
+                data: data
+            });
+        }
+        else
+        {
+            return res.status(404).json({
+                status: "error",
+                message: "Data not found"
+            });
+        }
+    }catch (error){
+        console.log(error);
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+const insert = async (req, res) => {
+    const request = req.body;
+
+    try{
+
+    }catch (error){
+        console.log(error);
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+const update = async (req, res) => {
+    const request = req.body;
+    const { id } = req.params;
+
+    try{
+        const data = await models.Salaries.findByPk(id);
+        if(!data){
+            console.log(123);
+            console.log(data);
+            return res.status(404).json({
+                status: "error",
+                message: "Data not found"
+            });
+        }
+        else{
+            console.log(data);
+            return res.status(200).json({
+                status: "success",
+                data: data
+            })
+        }
+    } catch (error) {
+        console.log(erorr);
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
 module.exports = {
-    index
+    index,
+    show,
+    insert,
+    update
 }
--- routes/salaryRoute.js
+++ routes/salaryRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { index } = require("../Controllers/transactionController");
+const { index, show, insert, update } = require("../Controllers/transactionController");
 
 router.route('/index').get(index);
+router.route('/show/:id').get(show);
+router.route('/create').post(insert);
+router.route('/update/:id').put(update);
 
 module.exports = router;
