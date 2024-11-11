12ec8ad06de7f59b87da4760dce78645846c1f2f
Tjoa <100271617+MarcelinoHansSetiaBudi@users.noreply.github.com>
2024-11-11 21:52:14 +0700

    Add Expanditure API

--- Controllers/expanditureController.js
+++ Controllers/expanditureController.js
@@ -16,29 +16,29 @@ const index = async (req, res) => {
 };
 
 const show = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try{
-        const data = await models.Expanditure.findByPk(id);
-        
-        if(!data) {
-            return res.status(404).json({
-                status: "error",
-                message: "Data not found"
-            });
-        }
+  try {
+    const data = await models.Expanditure.findByPk(id);
 
-        return res.status(200).json({
-            status: "success", 
-            data: data
-        });
-    }catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: error.message
-        });
+    if (!data) {
+      return res.status(404).json({
+        status: "error",
+        message: "Data not found",
+      });
     }
-}
+
+    return res.status(200).json({
+      status: "success",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 const insert = async (req, res) => {
   let request = req.body;
@@ -51,10 +51,10 @@ const insert = async (req, res) => {
       });
     }
     const insert = await models.Expanditure.create({
-      expanditure_type  : request.expanditure_type,
-      amount            : request.amount,
-      date              : request.date,
-      created_by        : request.created_by,
+      expanditure_type: request.expanditure_type,
+      amount: request.amount,
+      date: request.date,
+      created_by: request.created_by,
     });
 
     return res.status(200).json({
@@ -69,8 +69,70 @@ const insert = async (req, res) => {
   }
 };
 
+const update = async (req, res) => {
+    const request = req.body;
+    const { id } = req.params;
+
+    try{
+        if(!request.length){
+            return res.status(404).json({
+                status: "error",
+                message: "Data not found"
+            });
+        }
+
+        const data = await models.Expanditre.findByPk(id);
+
+        if(request.transactionId != '')
+        {
+            data.transactionId = request.transactionId;
+        }
+
+        data.expanditure_type   =   request.expanditure_type;
+        data.amount             =   request.amount;
+        data.date               =   request.date;
+
+        await data.update();
+
+        return res.status(200).json({
+            status: "success",
+            data: data
+        });
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+const destroy = async (req, res) => {
+    const { id } = req.params;
+    try{
+        const data = await models.Expanditure.findByPk(id);
+        if(!data){
+            return res.status(404).json({
+                status: "error",
+                message: "Data not found"
+            });
+        }
+
+        await data.destroy();
+        return res.status(200).json({
+            status: "success",
+            message: "Data deleted successfully"
+        });
+    }catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
 module.exports = {
   index,
   show,
   insert,
+  update,
+  destroy
 };
--- app.js
+++ app.js
@@ -13,11 +13,13 @@ const { authenticateToken } = require("./middleware/auth");
 // router
 const userRouter = require("./routes/userRoute");
 const transactionRouter = require("./routes/transactionRoute");
-const salaryRoute = require("./routes/salaryRoute")
+const salaryRoute = require("./routes/salaryRoute");
+const expanditureRoute = require("./routes/expanditureRoute");
 
 app.use("/api/users", userRouter);
 app.use("/api/transaction", authenticateToken, transactionRouter);
 app.use("/api/salaries", authenticateToken, salaryRoute);
+app.use("/api/expanditures", authenticateToken, expanditureRoute);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port " + process.env.PORT);
